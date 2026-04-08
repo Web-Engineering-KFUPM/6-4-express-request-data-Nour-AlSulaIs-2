894990df204ad90e67291f23730324c98f5ef1bf
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-04-08 11:26:01 +0300

    todo2

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -115,7 +115,17 @@ app.listen(3000, () => {
 
 
 // Query params: /echo?name=Ali&age=22
+// Query params: /echo?name=Ali&age=22
 
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+
+  if (!name || !age) {
+    return res.status(400).json({ok: false,error: "name & age required",});
+  }
+
+  res.json({ ok: true, name, age, msg: `Hello ${name}, you are ${age}`,});
+});
 
 // Route params: /profile/First/Last
 
